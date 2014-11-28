f3e6c9bdfa6deeda45c6370a7af79f1e91a6070e
Ben Immanuel <ben.immanuel@aistemos.com>
2014-11-28 12:44:56 +0000

    Finished up till "fetching from server"

--- template.js
+++ template.js
@@ -1,14 +1,64 @@
+// Eventually, data will come from the server
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
       </div>
     );
   }
 });
 
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var converter = new Showdown.converter(); // Showdown (converts mardown to HTML)
+var Comment = React.createClass({
+    render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());  
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});      
+
 React.render(
-  <CommentBox />,
-  document.getElementById('content')
+  <CommentBox data={data} />,                     // a React component
+  document.getElementById('content')  // the target container
 );
